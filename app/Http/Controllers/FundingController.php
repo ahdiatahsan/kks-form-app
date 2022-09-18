c7a4a2c3df701961dba70526f9212eff9394e846
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 01:59:31 +0800

    add funding model --all

---
+++ app/Http/Controllers/FundingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Funding;
+use App\Http\Requests\StoreFundingRequest;
+use App\Http\Requests\UpdateFundingRequest;
+
+class FundingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreFundingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFundingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Funding  $funding
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Funding $funding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Funding  $funding
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Funding $funding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateFundingRequest  $request
+     * @param  \App\Models\Funding  $funding
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateFundingRequest $request, Funding $funding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Funding  $funding
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Funding $funding)
+    {
+        //
+    }
+}
