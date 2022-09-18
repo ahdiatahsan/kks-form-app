90df0a246ecc09ff5063816a64362b697bc64e50
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:37:55 +0800

    add attachment one table --all

---
+++ app/Http/Controllers/AttachmentOneController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttachmentOne;
+use App\Http\Requests\StoreAttachmentOneRequest;
+use App\Http\Requests\UpdateAttachmentOneRequest;
+
+class AttachmentOneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttachmentOneRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttachmentOneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AttachmentOne  $attachmentOne
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttachmentOne $attachmentOne)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttachmentOne  $attachmentOne
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttachmentOne $attachmentOne)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttachmentOneRequest  $request
+     * @param  \App\Models\AttachmentOne  $attachmentOne
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttachmentOneRequest $request, AttachmentOne $attachmentOne)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttachmentOne  $attachmentOne
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttachmentOne $attachmentOne)
+    {
+        //
+    }
+}
