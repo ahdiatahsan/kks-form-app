b5dd05a349ab59e9f9afb43a4b1a3a5e9184ad91
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:36:23 +0800

    add tatanan note model --all

---
+++ app/Http/Controllers/TatananNoteController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TatananNote;
+use App\Http\Requests\StoreTatananNoteRequest;
+use App\Http\Requests\UpdateTatananNoteRequest;
+
+class TatananNoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTatananNoteRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTatananNoteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TatananNote  $tatananNote
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TatananNote $tatananNote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TatananNote  $tatananNote
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TatananNote $tatananNote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTatananNoteRequest  $request
+     * @param  \App\Models\TatananNote  $tatananNote
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTatananNoteRequest $request, TatananNote $tatananNote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TatananNote  $tatananNote
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TatananNote $tatananNote)
+    {
+        //
+    }
+}
---
+++ app/Models/TatananNote.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TatananNote extends Model
+{
+    use HasFactory;
+}
