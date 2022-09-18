7dbffbd2dc795f50a8337d51cd4a2f3601481198
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:44:24 +0800

    add attachment nine model --all

---
+++ app/Http/Controllers/AttachmentNineController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttachmentNine;
+use App\Http\Requests\StoreAttachmentNineRequest;
+use App\Http\Requests\UpdateAttachmentNineRequest;
+
+class AttachmentNineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttachmentNineRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttachmentNineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AttachmentNine  $attachmentNine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttachmentNine $attachmentNine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttachmentNine  $attachmentNine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttachmentNine $attachmentNine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttachmentNineRequest  $request
+     * @param  \App\Models\AttachmentNine  $attachmentNine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttachmentNineRequest $request, AttachmentNine $attachmentNine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttachmentNine  $attachmentNine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttachmentNine $attachmentNine)
+    {
+        //
+    }
+}
