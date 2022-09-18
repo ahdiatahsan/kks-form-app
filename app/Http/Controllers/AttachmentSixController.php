1456a425298244a7e9f66fde015a2027db2a4632
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:42:13 +0800

    add attachment six model --all

---
+++ app/Http/Controllers/AttachmentSixController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttachmentSix;
+use App\Http\Requests\StoreAttachmentSixRequest;
+use App\Http\Requests\UpdateAttachmentSixRequest;
+
+class AttachmentSixController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttachmentSixRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttachmentSixRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AttachmentSix  $attachmentSix
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttachmentSix $attachmentSix)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttachmentSix  $attachmentSix
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttachmentSix $attachmentSix)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttachmentSixRequest  $request
+     * @param  \App\Models\AttachmentSix  $attachmentSix
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttachmentSixRequest $request, AttachmentSix $attachmentSix)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttachmentSix  $attachmentSix
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttachmentSix $attachmentSix)
+    {
+        //
+    }
+}
