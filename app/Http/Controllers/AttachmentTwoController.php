ad4ea26f69134f9ff31cc20bf8fa0409f636a9f0
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:38:28 +0800

    add attachment two model --all

---
+++ app/Http/Controllers/AttachmentTwoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttachmentTwo;
+use App\Http\Requests\StoreAttachmentTwoRequest;
+use App\Http\Requests\UpdateAttachmentTwoRequest;
+
+class AttachmentTwoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttachmentTwoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttachmentTwoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AttachmentTwo  $attachmentTwo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttachmentTwo $attachmentTwo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttachmentTwo  $attachmentTwo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttachmentTwo $attachmentTwo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttachmentTwoRequest  $request
+     * @param  \App\Models\AttachmentTwo  $attachmentTwo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttachmentTwoRequest $request, AttachmentTwo $attachmentTwo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttachmentTwo  $attachmentTwo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttachmentTwo $attachmentTwo)
+    {
+        //
+    }
+}
