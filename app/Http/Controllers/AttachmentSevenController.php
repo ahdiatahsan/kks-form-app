ffa1bd2242c69e9a11b726ce4568238c075b44e2
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 02:42:43 +0800

    add attachment seven model --all

---
+++ app/Http/Controllers/AttachmentSevenController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AttachmentSeven;
+use App\Http\Requests\StoreAttachmentSevenRequest;
+use App\Http\Requests\UpdateAttachmentSevenRequest;
+
+class AttachmentSevenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAttachmentSevenRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAttachmentSevenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AttachmentSeven  $attachmentSeven
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttachmentSeven $attachmentSeven)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AttachmentSeven  $attachmentSeven
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttachmentSeven $attachmentSeven)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAttachmentSevenRequest  $request
+     * @param  \App\Models\AttachmentSeven  $attachmentSeven
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAttachmentSevenRequest $request, AttachmentSeven $attachmentSeven)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AttachmentSeven  $attachmentSeven
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttachmentSeven $attachmentSeven)
+    {
+        //
+    }
+}
