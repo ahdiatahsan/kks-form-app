e9ff54dbd13b1c50d6a8c38bd698d9567245c446
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-19 01:54:40 +0800

    add conclusion model --all

---
+++ app/Http/Controllers/ConclusionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conclusion;
+use App\Http\Requests\StoreConclusionRequest;
+use App\Http\Requests\UpdateConclusionRequest;
+
+class ConclusionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreConclusionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreConclusionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Conclusion  $conclusion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Conclusion $conclusion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Conclusion  $conclusion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Conclusion $conclusion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateConclusionRequest  $request
+     * @param  \App\Models\Conclusion  $conclusion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateConclusionRequest $request, Conclusion $conclusion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Conclusion  $conclusion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Conclusion $conclusion)
+    {
+        //
+    }
+}
