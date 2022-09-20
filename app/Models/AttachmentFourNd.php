9c5715718e952ded8a2c77079aa8806f2f01d320
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 04:50:39 +0800

    add second attachment models

---
+++ app/Models/AttachmentFourNd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttachmentFourNd extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/AttachmentOneNd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AttachmentOneNd extends Model
+{
+    use HasFactory;
+}
