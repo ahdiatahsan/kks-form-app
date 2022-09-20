6a8aaa3fb70a865538978f90b79f0114d78e85b2
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 05:07:39 +0800

    add fillable in second attachment models

--- app/Models/AttachmentFourNd.php
+++ app/Models/AttachmentFourNd.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class AttachmentFourNd extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'p1',
+        'p2',
+        'p3',
+        'p4',
+        'p5',
+        'p6',
+        'p7',
+        'p8',
+        'p9',
+        'p10',
+        'p11',
+        'p12',
+        'p13',
+        'p14',
+        'p15',
+        'p16',
+        'user_id'
+    ];
 }
