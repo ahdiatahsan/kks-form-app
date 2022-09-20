5d329282d8c1cd07cdb082afa1ab246c31d8d953
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 00:28:49 +0800

    add tatanan nine scheme

--- app/Models/TatananNine.php
+++ app/Models/TatananNine.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class TatananNine extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'p1',
+        'p2',
+        'p3',
+        'p4',
+        'p5',
+        'p6',
+        'p7',
+        'p8',
+        'p9',
+        'p10',
+        'p11',
+        'p12',
+        'p13',
+        'p14',
+        'p15',
+        'p16',
+        'p17',
+        'p18',
+        'p19',
+        'p20',
+        'p21',
+        'p22',
+        'p23',
+        'user_id'
+    ];
 }
