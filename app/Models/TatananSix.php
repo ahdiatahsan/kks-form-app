f196c5a214948b7c7b8ca2239c013607ecc8905f
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 00:28:15 +0800

    add tatanan six scheme

--- app/Models/TatananSix.php
+++ app/Models/TatananSix.php
@@ -8,4 +8,56 @@ use Illuminate\Database\Eloquent\Model;
 class TatananSix extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'p1',
+        'p2',
+        'p3',
+        'p4',
+        'p5',
+        'p6',
+        'p7',
+        'p8',
+        'p9',
+        'p10',
+        'p11',
+        'p12',
+        'p13',
+        'p14',
+        'p15',
+        'p16',
+        'p17',
+        'p18',
+        'p19',
+        'p20',
+        'p21',
+        'p22',
+        'p23',
+        'p24',
+        'p25',
+        'p26',
+        'p27',
+        'p28',
+        'p29',
+        'p30',
+        'p31',
+        'p32',
+        'p33',
+        'p34',
+        'p35',
+        'p36',
+        'p37',
+        'p38',
+        'p39',
+        'p40',
+        'p41',
+        'p42',
+        'p43',
+        'user_id'
+    ];
 }
