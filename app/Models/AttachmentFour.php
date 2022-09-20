f84444a523e32b2e4c136d732a35dea1238a6acc
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 00:56:51 +0800

    add attachment four scheme

--- app/Models/AttachmentFour.php
+++ app/Models/AttachmentFour.php
@@ -8,4 +8,45 @@ use Illuminate\Database\Eloquent\Model;
 class AttachmentFour extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'p1_1',
+        'p1_2',
+        'p2_1',
+        'p2_2',
+        'p3_1',
+        'p3_2',
+        'p4_1',
+        'p4_2',
+        'p5_1',
+        'p5_2',
+        'p6_1',
+        'p6_2',
+        'p7_1',
+        'p7_2',
+        'p8_1',
+        'p8_2',
+        'p9_1',
+        'p9_2',
+        'p10_1',
+        'p10_2',
+        'p11_1',
+        'p11_2',
+        'p12_1',
+        'p12_2',
+        'p13_1',
+        'p13_2',
+        'p14_1',
+        'p14_2',
+        'p15_1',
+        'p15_2',
+        'p16_1',
+        'p16_2',
+        'user_id'
+    ];
 }
