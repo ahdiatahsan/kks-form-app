b517437f6f76c54aac11c9ecb436f506513c8d8a
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 00:28:27 +0800

    add tatanan seven scheme

--- database/migrations/2022_09_13_064006_create_tatanan_sevens_table.php
+++ database/migrations/2022_09_13_064006_create_tatanan_sevens_table.php
@@ -15,6 +15,28 @@ return new class extends Migration
     {
         Schema::create('tatanan_sevens', function (Blueprint $table) {
             $table->id();
+            $table->string('p1')->nullable();
+            $table->string('p2')->nullable();
+            $table->string('p3')->nullable();
+            $table->string('p4')->nullable();
+            $table->string('p5')->nullable();
+            $table->string('p6')->nullable();
+            $table->string('p7')->nullable();
+            $table->string('p8')->nullable();
+            $table->string('p9')->nullable();
+            $table->string('p10')->nullable();
+            $table->string('p11')->nullable();
+            $table->string('p12')->nullable();
+            $table->string('p13')->nullable();
+            $table->string('p14')->nullable();
+            $table->string('p15')->nullable();
+            $table->string('p16')->nullable();
+            $table->string('p17')->nullable();
+            $table->string('p18')->nullable();
+            $table->string('p19')->nullable();
+            $table->string('p20')->nullable();
+            $table->string('p21')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
