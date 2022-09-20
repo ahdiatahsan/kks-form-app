fec53527e51cdb6e1a5562922931f9180841d6ae
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-21 00:28:04 +0800

    add tatanan five scheme

--- app/Models/TatananFive.php
+++ app/Models/TatananFive.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class TatananFive extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'p1',
+        'p2',
+        'p3',
+        'p4',
+        'p5',
+        'p6',
+        'p7',
+        'p8',
+        'p9',
+        'p10',
+        'p11',
+        'p12',
+        'p13',
+        'p14',
+        'p15',
+        'p16',
+        'p17',
+        'p18',
+        'p19',
+        'p20',
+        'p21',
+        'user_id'
+    ];
 }
