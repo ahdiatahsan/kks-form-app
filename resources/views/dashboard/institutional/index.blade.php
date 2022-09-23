710d89c3a79db74c5a80051d19bfe23e1ccd011e
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-24 02:36:00 +0800

    add institutional menu index

---
+++ resources/views/dashboard/institutional/index.blade.php
@@ -0,0 +1,113 @@
+<x-dashboard.layout>
+    {{-- Start::Head component --}}
+    <x-slot name="title">Kelembagaan</x-slot>
+    {{-- End::Head component --}}
+
+    {{-- Start::Toolbar component --}}
+    <x-slot name="toolbar">
+        <x-dashboard.toolbar.main>
+            <x-slot name="title">Kelembagaan</x-slot>
+
+            <x-slot name="breadcrumb">
+            </x-slot>
+
+            <x-slot name="backbutton">
+            </x-slot>
+        </x-dashboard.toolbar.main>
+    </x-slot>
+    {{-- End::Toolbar component --}}
+
+    {{-- Start::Post component --}}
+    <x-slot name="post">
+        <div class="row gy-5 g-xl-8">
+            <div class="col-xl-6">
+                <!--begin::Tim Pembina-->
+                <a href="#" class="card bg-success hoverable card-xl-stretch mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm002.svg-->
+                        <span class="svg-icon svg-icon-white svg-icon-3x ms-n1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z" fill="black"/>
+                                <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black"/>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <div class="text-white fw-bolder fs-2 mb-2 mt-5">Tim Pembina</div>
+                        <div class="fw-bold text-white">Tim Pembina KKS Enrekang</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+                <!--end::Tim Pembina-->
+            </div>
+            <div class="col-xl-6">
+                <!--begin::Form KKS-->
+                <a href="#" class="card bg-primary hoverable card-xl-stretch mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm008.svg-->
+                        <span class="svg-icon svg-icon-white svg-icon-3x ms-n1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z" fill="black"/>
+                                <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black"/>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <div class="text-white fw-bolder fs-2 mb-2 mt-5">Form KKS</div>
+                        <div class="fw-bold text-white">Form KKS Enrekang</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+                <!--end::Form KKS-->
+            </div>
+        </div>
+        <div class="row gy-5 g-xl-8">
+            <div class="col-xl-6">
+                <!--begin::Form Kecamatan-->
+                <a href="#" class="card bg-primary hoverable card-xl-stretch mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm002.svg-->
+                        <span class="svg-icon svg-icon-white svg-icon-3x ms-n1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z" fill="black"/>
+                                <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black"/>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <div class="text-white fw-bolder fs-2 mb-2 mt-5">Form Kecamatan</div>
+                        <div class="fw-bold text-white">Form Kecamatan KKS Enrekang</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+                <!--end::Form Kecamatan-->
+            </div>
+            <div class="col-xl-6">
+                <!--begin::Form Desa-->
+                <a href="#" class="card bg-success hoverable card-xl-stretch mb-xl-8">
+                    <!--begin::Body-->
+                    <div class="card-body">
+                        <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm008.svg-->
+                        <span class="svg-icon svg-icon-white svg-icon-3x ms-n1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z" fill="black"/>
+                                <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black"/>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                        <div class="text-white fw-bolder fs-2 mb-2 mt-5">Form Desa</div>
+                        <div class="fw-bold text-white">Form Desa KKS Enrekang</div>
+                    </div>
+                    <!--end::Body-->
+                </a>
+                <!--end::Form Desa-->
+            </div>
+        </div>
+    </x-slot>
+    {{-- End::Post component --}}
+
+    {{-- Start::Javascript --}}
+    
+    {{-- End::Javascript --}}
+
+</x-dashboard.layout>
