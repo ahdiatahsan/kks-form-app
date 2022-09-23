05be71f64838a6e1330df9eafc546f34268f8590
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-24 02:35:23 +0800

    add more route resources

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IntroductionController;
+use App\Http\Controllers\InstitutionalBuilderController;
+use App\Http\Controllers\InstitutionalKksController;
+use App\Http\Controllers\InstitutionalDistrictController;
+use App\Http\Controllers\InstitutionalVillageController;
+use App\Http\Controllers\FundingController;
+use App\Http\Controllers\TatananMainController;
+use App\Http\Controllers\TatananNoteController;
 use App\Http\Controllers\TatananOneController;
 use App\Http\Controllers\TatananTwoController;
 use App\Http\Controllers\TatananThreeController;
@@ -10,6 +18,7 @@ use App\Http\Controllers\TatananSixController;
 use App\Http\Controllers\TatananSevenController;
 use App\Http\Controllers\TatananEightController;
 use App\Http\Controllers\TatananNineController;
+use App\Http\Controllers\ConclusionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -31,7 +40,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
+Route::get('/indikator-khusus', [DashboardController::class, 'tatanan_menu'])->name('tatanan-menu');
+Route::get('/kelembagaan', [DashboardController::class, 'institutional_menu'])->name('institutional-menu');
 
+Route::resource('pendahuluan', IntroductionController::class);
+Route::resource('pendanaan', FundingController::class);
+Route::resource('penutup', ConclusionController::class);
+
+Route::resource('tim-pembina', InstitutionalBuilderController::class);
+Route::resource('form-kks', InstitutionalKksController::class);
+Route::resource('form-kecamatan', InstitutionalDistrictController::class);
+Route::resource('form-desa', InstitutionalVillageController::class);
+
+Route::resource('indikator-pokok', TatananMainController::class);
+Route::resource('tatanan-catatan', TatananNoteController::class);
+Route::resource('tatanan-satu', TatananOneController::class);
 Route::resource('tatanan-satu', TatananOneController::class);
 Route::resource('tatanan-dua', TatananTwoController::class);
 Route::resource('tatanan-tiga', TatananThreeController::class);
