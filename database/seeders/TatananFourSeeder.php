f2a477a436dabd1eab07b62b77f78f2edc634cab
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-26 00:26:26 +0800

    add tatanan seeders

--- database/seeders/TatananFourSeeder.php
+++ database/seeders/TatananFourSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\TatananFour;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,80 @@ class TatananFourSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $TatananFours = [
+            [
+                'user_id' => 1
+            ],
+            [
+                'user_id' => 2
+            ],
+            [
+                'user_id' => 3
+            ],
+            [
+                'user_id' => 4
+            ],
+            [
+                'user_id' => 5
+            ],
+            [
+                'user_id' => 6
+            ],
+            [
+                'user_id' => 7
+            ],
+            [
+                'user_id' => 8
+            ],
+            [
+                'user_id' => 9
+            ],
+            [
+                'user_id' => 10
+            ],
+            [
+                'user_id' => 11
+            ],
+            [
+                'user_id' => 12
+            ],
+            [
+                'user_id' => 13
+            ],
+            [
+                'user_id' => 14
+            ],
+            [
+                'user_id' => 15
+            ],
+            [
+                'user_id' => 16
+            ],
+            [
+                'user_id' => 17
+            ],
+            [
+                'user_id' => 18
+            ],
+            [
+                'user_id' => 19
+            ],
+            [
+                'user_id' => 20
+            ],
+            [
+                'user_id' => 21
+            ],
+            [
+                'user_id' => 22
+            ],
+            [
+                'user_id' => 23
+            ],
+        ];
+
+        foreach($TatananFours as $TatananFour){
+            TatananFour::create($TatananFour);
+        }
     }
 }
