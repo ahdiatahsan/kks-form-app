da7733c48f114da7e47878b1c088da3c1c5325d9
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-26 00:27:21 +0800

    add seeder classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,37 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+
+            TatananOneSeeder::class,
+            TatananTwoSeeder::class,
+            TatananThreeSeeder::class,
+            TatananFourSeeder::class,
+            TatananFiveSeeder::class,
+            TatananSixSeeder::class,
+            TatananSevenSeeder::class,
+            TatananEightSeeder::class,
+            TatananNineSeeder::class,
+            
+            AttachmentOneSeeder::class,
+            AttachmentTwoSeeder::class,
+            AttachmentThreeSeeder::class,
+            AttachmentFourSeeder::class,
+            AttachmentFiveSeeder::class,
+            AttachmentSixSeeder::class,
+            AttachmentSevenSeeder::class,
+            AttachmentEightSeeder::class,
+            AttachmentNineSeeder::class,
+
+            AttachmentOneNdSeeder::class,
+            AttachmentTwoNdSeeder::class,
+            AttachmentThreeNdSeeder::class,
+            AttachmentFourNdSeeder::class,
+            AttachmentFiveNdSeeder::class,
+            AttachmentSixNdSeeder::class,
+            AttachmentSevenNdSeeder::class,
+            AttachmentEightNdSeeder::class,
+            AttachmentNineNdSeeder::class,
         ]);
     }
 }
