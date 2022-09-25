72e330b2c79a232fa7a01d885c6afe7ac8ecff90
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-25 20:20:48 +0800

    disable reset and verify auth routes

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,9 @@ Route::get('/', function () {
 })->middleware('auth');
 
 Auth::routes([
-    'register' => false,
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
 ]);
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('home');
