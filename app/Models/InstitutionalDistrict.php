226dbf879578cba912b82656531509a90506b869
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-09-30 22:25:43 +0800

    add institutional district fields and relations

--- app/Models/InstitutionalDistrict.php
+++ app/Models/InstitutionalDistrict.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class InstitutionalDistrict extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'description',
+        'attachment_pdf',
+        'attachment_img',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
