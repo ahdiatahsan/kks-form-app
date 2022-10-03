774e3aa9f319f9705920213e19bc441df1bbcb79
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-04 01:36:09 +0800

    add notes models

---
+++ app/Models/NoteFour.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NoteFour extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/NoteTwo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NoteTwo extends Model
+{
+    use HasFactory;
+}
