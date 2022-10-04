e866501e2fcbc28abfb409172390eae90c03f3ab
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-05 01:43:15 +0800

    edit footer text

--- resources/views/components/dashboard/footer.blade.php
+++ resources/views/components/dashboard/footer.blade.php
@@ -3,8 +3,8 @@
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
-            <span class="text-muted fw-bold me-1">2022 © </span>
-            <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+            <span class="text-muted fw-bold me-1">{{ date('Y') }} &copy; </span>
+            <span class="text-gray-800 text-hover-primary">KKS Enrekang</span>
         </div>
         <!--end::Copyright-->
     </div>
