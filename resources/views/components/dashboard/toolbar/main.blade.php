64947d02d05f2f56bb90ead3d7d4bee7fdda1549
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-04 23:44:07 +0800

    add flex row reverse and order

--- resources/views/components/dashboard/toolbar/main.blade.php
+++ resources/views/components/dashboard/toolbar/main.blade.php
@@ -1,8 +1,8 @@
 <div class="toolbar" id="kt_toolbar">
     <!--begin::Container-->
-    <div id="kt_toolbar_container" class="container-xxl d-flex flex-stack">
+    <div id="kt_toolbar_container" class="container-xxl d-flex flex-stack flex-row-reverse">
         <!--begin::Page title-->
-        <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+        <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0 order-2">
             <!--begin::Title-->
             <h1 class="d-flex align-items-center text-dark fw-bolder fs-5 my-1">{{ $title }}</h1>
             <!--end::Title-->
@@ -17,7 +17,7 @@
         </div>
         <!--end::Page title-->
         <!--begin::Actions-->
-        <div class="d-flex align-items-center py-1">
+        <div class="d-flex align-items-center py-1 order-1">
             {{ $backbutton }}
         </div>
         <!--end::Actions-->
