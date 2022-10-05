c70aa30a92214606da156985ff9bdc5e3fbc3675
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-05 09:02:09 +0800

    add funding resource views

---
+++ resources/views/dashboard/funding/create.blade.php
@@ -0,0 +1,132 @@
+<x-dashboard.layout>
+    {{-- Start::Head component --}}
+    <x-slot name="title">Pendanaan</x-slot>
+    {{-- End::Head component --}}
+
+    {{-- Start::Toolbar component --}}
+    <x-slot name="toolbar">
+        <x-dashboard.toolbar.main>
+            <x-slot name="title">Pendanaan</x-slot>
+
+            <x-slot name="breadcrumb">
+                <li class="breadcrumb-item">
+                    <span class="text-muted">Form Tambah Data</span>
+                </li>
+            </x-slot>
+
+            <x-slot name="backbutton">
+                <!--begin::Button-->
+                <a href="{{ route('funding.index') }}" class="btn btn-sm btn-light-success fw-bolder">
+                    <span class="svg-icon svg-icon-muted svg-icon-3"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M9.60001 11H21C21.6 11 22 11.4 22 12C22 12.6 21.6 13 21 13H9.60001V11Z"
+                                fill="black" />
+                            <path opacity="0.3" d="M9.6 20V4L2.3 11.3C1.9 11.7 1.9 12.3 2.3 12.7L9.6 20Z"
+                                fill="black" />
+                        </svg></span> Beranda
+                </a>
+                <!--end::Button-->
+            </x-slot>
+        </x-dashboard.toolbar.main>
+    </x-slot>
+    {{-- End::Toolbar component --}}
+
+    {{-- Start::Post component --}}
+    <x-slot name="post">
+        <!--begin::form-->
+        <form class="form" action="{{ route('funding.store') }}" method="POST" autocomplete="off"
+            enctype="multipart/form-data">
+            @csrf
+            <!--begin::card-->
+            <div class="card">
+                <!--begin::Card header-->
+                <div class="card-header">
+                    <h3 class="card-title align-items-start flex-column">
+                        <span class="card-label fw-bolder text-dark fs-5 my-1">Pendanaan</span>
+                        <span class="text-muted mt-1 fw-bold fs-7">Form Tambah Data</span>
+                    </h3>
+                </div>
+                <!--end::Card header-->
+                <!--begin::Card body-->
+                <div class="card-body">
+                    <div class="mb-6">
+                        <label class="form-label fs-7 required">Judul</label>
+                        <div class="input-group input-group-sm">
+                            <input class="form-control" type="text" name="title" value="{{ old('title') }}"
+                                required>
+                        </div>
+                    </div>
+                    <div class="mb-6">
+                        <label class="form-label fs-7 required">Tanggal Kegiatan</label>
+                        <div class="input-group input-group-sm">
+                            <input class="form-control flatpickr" type="text" name="date_activity"
+                                value="{{ old('date_activity') }}" required>
+                        </div>
+                    </div>
+                    <div class="mb-6">
+                        <label class="form-label fs-7 required">Keterangan</label>
+                        <div class="input-group input-group-sm">
+                            <input class="form-control" type="text" name="description" value="{{ old('description') }}"
+                                required>
+                        </div>
+                    </div>
+                    <div class="mb-8">
+                        <label class="form-label">
+                            <span class="fs-7 required">Berkas Pendanaan</span>
+                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                title="Ukuran maksimal berkas (pdf) adalah 3MB."></i>
+                        </label>
+                        <div class="input-group input-group-sm">
+                            <input class="form-control" type="file" name="attachment_pdf" required>
+                        </div>
+                    </div>
+                    <div class="mb-8">
+                        <label class="form-label">
+                            <span class="fs-7 required">Dokumentasi</span>
+                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                title="Ukuran maksimal dokumentasi (jpg, jpeg, png, webp) adalah 2MB."></i>
+                        </label>
+                        <div class="input-group input-group-sm">
+                            <input class="form-control" type="file" name="attachment_img" required>
+                        </div>
+                    </div>
+                </div>
+                <!--end::Card body-->
+                <!--begin::Card footer-->
+                <div class="card-footer d-flex justify-content-end py-6">
+                    <button type="submit" class="btn btn-sm btn-primary">
+                        <span class="svg-icon svg-icon-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <path opacity="0.3"
+                                    d="M10 18C9.7 18 9.5 17.9 9.3 17.7L2.3 10.7C1.9 10.3 1.9 9.7 2.3 9.3C2.7 8.9 3.29999 8.9 3.69999 9.3L10.7 16.3C11.1 16.7 11.1 17.3 10.7 17.7C10.5 17.9 10.3 18 10 18Z"
+                                    fill="black" />
+                                <path
+                                    d="M10 18C9.7 18 9.5 17.9 9.3 17.7C8.9 17.3 8.9 16.7 9.3 16.3L20.3 5.3C20.7 4.9 21.3 4.9 21.7 5.3C22.1 5.7 22.1 6.30002 21.7 6.70002L10.7 17.7C10.5 17.9 10.3 18 10 18Z"
+                                    fill="black" />
+                            </svg>
+                        </span>Simpan</button>
+                </div>
+                <!--end::Card footer-->
+            </div>
+            <!--end::card-->
+        </form>
+        <!--end::form-->
+    </x-slot>
+    {{-- End::Post component --}}
+
+    {{-- Start::Javascript --}}
+    <x-slot name="javascript">
+        {{-- Start::Flatpickr --}}
+        <script src="{{ asset('js/custom/flatpickr/id.js') }}"></script>
+        <script>
+            $(".flatpickr").flatpickr({
+                altInput: true,
+                locale: "id",
+                altFormat: "d F Y",
+            });
+        </script>
+        {{-- End::Flatpickr --}}
+    </x-slot>
+    {{-- End::Javascript --}}
+</x-dashboard.layout>
---
+++ resources/views/dashboard/funding/index.blade.php
@@ -0,0 +1,190 @@
+<x-dashboard.layout>
+    {{-- Start::Head component --}}
+    <x-slot name="title">Pendanaan</x-slot>
+
+    <x-slot name="style">
+        <link rel="stylesheet" href="{{ asset('plugins/custom/datatables/datatables.bundle.css') }} ">
+    </x-slot>
+    {{-- End::Head component --}}
+
+    {{-- Start::Toolbar component --}}
+    <x-slot name="toolbar">
+        <x-dashboard.toolbar.main>
+            <x-slot name="title">Pendanaan</x-slot>
+
+            <x-slot name="breadcrumb">
+                <li class="breadcrumb-item">
+                    <span class="text-muted">Pendanaan</span>
+                </li>
+            </x-slot>
+
+            <x-slot name="backbutton">
+            </x-slot>
+        </x-dashboard.toolbar.main>
+    </x-slot>
+    {{-- End::Toolbar component --}}
+
+    {{-- Start::Post component --}}
+    <x-slot name="post">
+        <!--begin::Card-->
+        <div class="card">
+            <!--begin::Card header-->
+            <div class="card-header">
+                <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label fw-bolder text-dark fs-5 my-1">Pendanaan</span>
+                    <span class="text-muted mt-1 fw-bold fs-7">Beranda</span>
+                </h3>
+                <div class="card-toolbar">
+                    @if (Auth::user()->hasAnyRole('administrator|office'))
+                    <a href="{{ route('funding.create') }}" class="btn btn-sm btn-light-primary">
+                        <span class="svg-icon svg-icon-muted svg-icon-3"><svg xmlns="http://www.w3.org/2000/svg"
+                                width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <rect opacity="0.5" x="11.364" y="20.364" width="16" height="2"
+                                    rx="1" transform="rotate(-90 11.364 20.364)" fill="black" />
+                                <rect x="4.36396" y="11.364" width="16" height="2" rx="1"
+                                    fill="black" />
+                            </svg></span> Tambah Pendanaan
+                    </a>
+                    @endif
+                </div>
+            </div>
+            <!--end::Card header-->
+            <!--begin::Card body-->
+            <div class="card-body">
+                <table id="datatable"
+                    class="table table-sm table-row-bordered gy-5 gs-4 border rounded no-footer nowrap dataTable">
+                    <thead>
+                        <tr class="border-gray-200 fw-bold bg-lighten fs-7">
+                            <th class="min-w-30px">No.</th>
+                            <th class="min-w-150px">Judul</th>
+                            <th class="min-w-150px">Tanggal Kegiatan</th>
+                            <th class="min-w-150px">Keterangan</th>
+                            <th class="min-w-150px">OPD</th>
+                            <th class="min-w-100px">Berkas</th>
+                            <th class="text-end min-w-100px">Opsi</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <!--end::Card body-->
+        </div>
+        <!--end::Card-->
+    </x-slot>
+    {{-- End::Post component --}}
+
+    {{-- Start::Javascript --}}
+    <x-slot name="javascript">
+        {{-- Start::Sweetalert2 --}}
+        <script>
+            $(document).on('click', '.delete', function() {
+                var id = $(this).attr('id');
+                swal.fire({
+                    title: "Hapus data ini?",
+                    text: "Data yang terhapus tidak dapat dikembalikan.",
+                    icon: "warning",
+                    showCloseButton: true,
+                    confirmButtonColor: '#f1416c',
+                    confirmButtonText: 'Ya, Hapus!'
+                }).then(function(result) {
+                    if (result.value) {
+                        var url = '{{ route("funding.destroy", ":funding") }}';
+                        url = url.replace(':funding', id);
+
+                        $.ajax({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            url: url,
+                            type: 'POST',
+                            datatype: 'json',
+                            data: {
+                                "_method": 'DELETE',
+                            },
+                            success: function(data) {
+                                setTimeout(function() {
+                                    location.reload()
+                                }, 1500);
+                            },
+                            complete: function() {
+                                swal.hideLoading();
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                swal.hideLoading();
+                                swal.fire("Maaf ", "Terjadi kesalahan", "error");
+                            }
+                        });
+                    }
+                });
+            });
+        </script>
+        {{-- End::Sweetalert2 --}}
+
+        {{-- Start::Datatable --}}
+        <script src="{{ asset('plugins/custom/datatables/datatables.bundle.js') }}"></script>
+        <script>
+            $(document).ready(function() {
+                $('#datatable').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ajax: "{{ route('funding.datatable') }}",
+                    columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    }, {
+                        data: 'title',
+                        name: 'title'
+                    }, {
+                        data: 'date_activity',
+                        name: 'date_activity'
+                    }, {
+                        data: 'description',
+                        name: 'description'
+                    }, {
+                        data: 'user.name',
+                        name: 'user.name'
+                    }, {
+                        data: 'attachment',
+                        name: 'attachment'
+                    }, {
+                        data: 'action',
+                        name: 'action'
+                    }],
+                    columnDefs: [{
+                        className: 'text-center',
+                        orderable: false,
+                        targets: [0]
+                    }, {
+                        className: 'text-center mw-100px',
+                        orderable: false,
+                        targets: [5,6]
+                    }],
+                    pagingType: "full_numbers",
+                    "language": {
+                        "search": "Cari:",
+                        "emptyTable": "Tidak ada data yang tersedia pada tabel ini",
+                        "info": "Menampilkan _START_ - _END_ dari _TOTAL_ data",
+                        "infoEmpty": "Tidak menampilkan data apapun",
+                        "infoFiltered":   "(difilter dari total _MAX_ data)",
+                        "loadingRecords": "Memuat...",
+                        "processing": "Memproses...",
+                        "zeroRecords": "Tidak ada data yang cocok ditemukan",
+                    },
+                    dom: "<'row'" +
+                        "<'col-sm-6 d-flex align-items-center justify-conten-start'l>" +
+                        "<'col-sm-6 d-flex align-items-center justify-content-end z-0'f>" +
+                        ">" +
+
+                        "<'table-responsive'tr>" +
+
+                        "<'row'" +
+                        "<'col-sm-6 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'i>" +
+                        "<'col-sm-6 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'p>" +
+                        ">"
+                });
+            });
+        </script>
+        {{-- Start::Datatable --}}
+    </x-slot>
+    {{-- End::Javascript --}}
+
+</x-dashboard.layout>
