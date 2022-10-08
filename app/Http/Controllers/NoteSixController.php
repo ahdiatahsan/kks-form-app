f4b51afaa28735a9de6d4cc6e9461aa036723a34
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-08 10:18:23 +0800

    add functions in note six controller

--- app/Http/Controllers/NoteSixController.php
+++ app/Http/Controllers/NoteSixController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\NoteSix;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class NoteSixController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['permission:note-view'])->only(['index', 'show', 'datatable']);
+        $this->middleware(['permission:note-create'])->only(['create', 'store']);
+        $this->middleware(['permission:note-update'])->only(['edit', 'update']);
+        $this->middleware(['permission:note-delete'])->only(['destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,7 @@ class NoteSixController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.note.tatanan6.index');
     }
 
     /**
@@ -69,7 +78,14 @@ class NoteSixController extends Controller
      */
     public function update(Request $request, NoteSix $noteSix)
     {
-        //
+        $request->validate([
+            'note' => 'string'
+        ]);
+
+        $noteSix->note = $request->input('note');
+        $noteSix->save();
+
+        return redirect()->route('noteSix.index')->with('success', 'Catatan untuk pertanyaan nomor '. $noteSix->id .' berhasil disimpan.');
     }
 
     /**
@@ -81,5 +97,26 @@ class NoteSixController extends Controller
     public function destroy(NoteSix $noteSix)
     {
         //
+    }
+    
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $noteSixs = noteSix::with('user')->get();
+
+            return DataTables::of($noteSixs)
+                ->addColumn('attachment', function ($noteSix) {
+                    return view('dashboard.note.tatanan6.attachment', compact('noteSix'))->render();
+                })
+                ->addColumn('action', function ($noteSix) {
+                    return view('dashboard.note.tatanan6.action', compact('noteSix'))->render();
+                })
+                ->rawColumns(['attachment', 'action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
     }
 }
