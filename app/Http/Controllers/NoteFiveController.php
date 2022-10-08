f33d171dc55641a339f89349eed88f25a2ac09af
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-08 10:18:16 +0800

    add functions in note five controller

--- app/Http/Controllers/NoteFiveController.php
+++ app/Http/Controllers/NoteFiveController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\NoteFive;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class NoteFiveController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['permission:note-view'])->only(['index', 'show', 'datatable']);
+        $this->middleware(['permission:note-create'])->only(['create', 'store']);
+        $this->middleware(['permission:note-update'])->only(['edit', 'update']);
+        $this->middleware(['permission:note-delete'])->only(['destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,7 @@ class NoteFiveController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.note.tatanan5.index');
     }
 
     /**
@@ -69,7 +78,14 @@ class NoteFiveController extends Controller
      */
     public function update(Request $request, NoteFive $noteFive)
     {
-        //
+        $request->validate([
+            'note' => 'string'
+        ]);
+
+        $noteFive->note = $request->input('note');
+        $noteFive->save();
+
+        return redirect()->route('noteFive.index')->with('success', 'Catatan untuk pertanyaan nomor '. $noteFive->id .' berhasil disimpan.');
     }
 
     /**
@@ -81,5 +97,26 @@ class NoteFiveController extends Controller
     public function destroy(NoteFive $noteFive)
     {
         //
+    }
+    
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $noteFives = NoteFive::with('user')->get();
+
+            return DataTables::of($noteFives)
+                ->addColumn('attachment', function ($noteFive) {
+                    return view('dashboard.note.tatanan5.attachment', compact('noteFive'))->render();
+                })
+                ->addColumn('action', function ($noteFive) {
+                    return view('dashboard.note.tatanan5.action', compact('noteFive'))->render();
+                })
+                ->rawColumns(['attachment', 'action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
     }
 }
