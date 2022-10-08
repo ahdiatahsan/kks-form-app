2ccdb0d332d2fba8cb21560f1bec2a893d230ed2
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-08 10:18:44 +0800

    add functions in note nine controller

--- app/Http/Controllers/NoteNineController.php
+++ app/Http/Controllers/NoteNineController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\NoteNine;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class NoteNineController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['permission:note-view'])->only(['index', 'show', 'datatable']);
+        $this->middleware(['permission:note-create'])->only(['create', 'store']);
+        $this->middleware(['permission:note-update'])->only(['edit', 'update']);
+        $this->middleware(['permission:note-delete'])->only(['destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,7 @@ class NoteNineController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.note.tatanan9.index');
     }
 
     /**
@@ -69,7 +78,14 @@ class NoteNineController extends Controller
      */
     public function update(Request $request, NoteNine $noteNine)
     {
-        //
+        $request->validate([
+            'note' => 'string'
+        ]);
+
+        $noteNine->note = $request->input('note');
+        $noteNine->save();
+
+        return redirect()->route('noteNine.index')->with('success', 'Catatan untuk pertanyaan nomor '. $noteNine->id .' berhasil disimpan.');
     }
 
     /**
@@ -81,5 +97,26 @@ class NoteNineController extends Controller
     public function destroy(NoteNine $noteNine)
     {
         //
+    }
+
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $noteNines = NoteNine::with('user')->get();
+
+            return DataTables::of($noteNines)
+                ->addColumn('attachment', function ($noteNine) {
+                    return view('dashboard.note.tatanan9.attachment', compact('noteNine'))->render();
+                })
+                ->addColumn('action', function ($noteNine) {
+                    return view('dashboard.note.tatanan9.action', compact('noteNine'))->render();
+                })
+                ->rawColumns(['attachment', 'action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
     }
 }
