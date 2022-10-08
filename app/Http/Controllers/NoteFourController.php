d314d1fcb73f50478e3b68de7643338732988e66
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-08 10:18:10 +0800

    add functions in note four controller

--- app/Http/Controllers/NoteFourController.php
+++ app/Http/Controllers/NoteFourController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\NoteFour;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class NoteFourController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['permission:note-view'])->only(['index', 'show', 'datatable']);
+        $this->middleware(['permission:note-create'])->only(['create', 'store']);
+        $this->middleware(['permission:note-update'])->only(['edit', 'update']);
+        $this->middleware(['permission:note-delete'])->only(['destroy']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,7 @@ class NoteFourController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.note.tatanan4.index');
     }
 
     /**
@@ -69,7 +78,14 @@ class NoteFourController extends Controller
      */
     public function update(Request $request, NoteFour $noteFour)
     {
-        //
+        $request->validate([
+            'note' => 'string'
+        ]);
+
+        $noteFour->note = $request->input('note');
+        $noteFour->save();
+
+        return redirect()->route('noteFour.index')->with('success', 'Catatan untuk pertanyaan nomor '. $noteFour->id .' berhasil disimpan.');
     }
 
     /**
@@ -81,5 +97,26 @@ class NoteFourController extends Controller
     public function destroy(NoteFour $noteFour)
     {
         //
+    }
+
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $noteFours = NoteFour::with('user')->get();
+
+            return DataTables::of($noteFours)
+                ->addColumn('attachment', function ($noteFour) {
+                    return view('dashboard.note.tatanan4.attachment', compact('noteFour'))->render();
+                })
+                ->addColumn('action', function ($noteFour) {
+                    return view('dashboard.note.tatanan4.action', compact('noteFour'))->render();
+                })
+                ->rawColumns(['attachment', 'action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
     }
 }
