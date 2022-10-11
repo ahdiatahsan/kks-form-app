f78698ae96dc59a33aed3fadd88fa502c8afec56
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-10-12 03:22:51 +0800

    change login title text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <!--begin::Head-->
 
 <head>
-    <title>Aplikasi KKS Enrekang 2022 - Login</title>
+    <title>Aplikasi KKS Enrekang 2022 - Masuk</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width= device-width, initial-scale=1, shrink-to-fit=no" />
     <meta property="og:locale" content="id, in" />
