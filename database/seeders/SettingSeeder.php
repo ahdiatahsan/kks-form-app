85c62d73cf5026a0e5d8aaa935b94fd3cb274448
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-12-22 01:27:48 +0800

    add setting model --all

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::factory()->count(1)->create();
+    }
+}
