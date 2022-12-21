76c5ccd9850c058873dbb9ebcdb22f2674aaebf9
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-12-22 04:21:47 +0800

    add setting seeder class in dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             IntroductionSeeder::class,
             ConclusionSeeder::class,
+            SettingSeeder::class,
 
             TatananOneSeeder::class,
             TatananTwoSeeder::class,
