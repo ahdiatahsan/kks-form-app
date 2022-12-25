dbcb91a5400216539996ce21c46c3c37d553d75b
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-12-26 05:09:05 +0800

    update functions in conclusion controller

--- app/Http/Controllers/ConclusionController.php
+++ app/Http/Controllers/ConclusionController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Conclusion;
-use App\Http\Requests\StoreConclusionRequest;
-use App\Http\Requests\UpdateConclusionRequest;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Setting;
+// use App\Http\Requests\StoreConclusionRequest;
+// use App\Http\Requests\UpdateConclusionRequest;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class ConclusionController extends Controller
 {
@@ -26,8 +29,7 @@ class ConclusionController extends Controller
      */
     public function index()
     {
-        $conclusion = Conclusion::first();
-        return view('dashboard.conclusion.index', compact('conclusion'));
+        return view('dashboard.conclusion.index');
     }
 
     /**
@@ -37,7 +39,7 @@ class ConclusionController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.conclusion.create');
     }
 
     /**
@@ -46,9 +48,29 @@ class ConclusionController extends Controller
      * @param  \App\Http\Requests\StoreConclusionRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreConclusionRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'attachment' => 'required|file|max:3048|mimes:pdf',
+            'period' => 'required|numeric|unique:conclusions,setting_id'
+        ], [
+            'attachment.mimes' => 'Berkas harus berupa file pdf.',
+            'attachment.max' => 'Berkas tidak boleh lebih besar dari 3 MB',
+            'period.unique' => 'Penutup tahun periode tersebut telah ada sebelumnya.'
+        ]);
+        
+        $setting = Setting::where('id', '=', $request->input('period'))->first();
+        $attachFile = $request->file('attachment');
+        $attachName = 'Penutup KKS Enrekang ' . $setting->period . '.' . $attachFile->getClientOriginalExtension();
+        Storage::putFileAs('public/conclusion', $attachFile, $attachName);
+
+        Conclusion::create([
+            'attachment' => $attachName,
+            'body' => $request->input('body'),
+            'setting_id' => $request->input('period')
+        ]);
+
+        return redirect()->route('conclusion.create')->with('success', 'Data Penutup periode ' . $setting->period . ' berhasil ditambahkan.');
     }
 
     /**
@@ -59,7 +81,7 @@ class ConclusionController extends Controller
      */
     public function show(Conclusion $conclusion)
     {
-        //
+        return view('dashboard.conclusion.show', compact('conclusion'));
     }
 
     /**
@@ -82,12 +104,16 @@ class ConclusionController extends Controller
      */
     public function update(Request $request, Conclusion $conclusion)
     {
+        $setting = Setting::where('id', '=', $request->input('period'))->first();
+
         if ($request->file() != null) {
             $request->validate([
-                'attachment' => 'required|file|max:3048|mimes:pdf'
+                'attachment' => 'required|file|max:3048|mimes:pdf',
+                'period' => 'required|numeric|unique:conclusions,setting_id,' . $conclusion->id,
             ], [
                 'attachment.mimes' => 'Berkas harus berupa file pdf.',
-                'attachment.max' => 'Berkas tidak boleh lebih besar dari 3 MB'
+                'attachment.max' => 'Berkas tidak boleh lebih besar dari 3 MB',
+                'period.unique' => 'Penutup tahun periode tersebut telah ada sebelumnya.'
             ]);
 
             if (Storage::exists('public/conclusion/' . $conclusion->attachment)) {
@@ -96,16 +122,27 @@ class ConclusionController extends Controller
 
             $attachFile = $request->file('attachment');
             $extension = $attachFile->extension();
-            $attachName = 'Penutup KKS Enrekang ' . date('Y') . '.' . $extension;
+            $attachName = 'Penutup KKS Enrekang ' . $setting->period . '.' . $extension;
             Storage::putFileAs('public/conclusion', $attachFile, $attachName);
 
+            $conclusion->attachment = $attachName;
+        } else {
+            $request->validate([
+                'period' => 'required|numeric|unique:conclusions,setting_id,' . $conclusion->id
+            ], [
+                'period.unique' => 'Penutup tahun periode tersebut telah ada sebelumnya.'
+            ]);
+
+            $attachName = 'Penutup KKS Enrekang ' . $setting->period . '.pdf';
+            Storage::move('public/conclusion/' . $conclusion->attachment, 'public/conclusion/' . $attachName);
             $conclusion->attachment = $attachName;
         }
 
         $conclusion->body = $request->input('body');
+        $conclusion->setting_id = $request->input('period');
         $conclusion->save();
 
-        return redirect()->route('conclusion.edit', $conclusion->id)->with('success', 'Data Penutup Berhasil Disimpan.');
+        return redirect()->route('conclusion.edit', $conclusion->id)->with('success', 'Data Penutup telah diubah.');
     }
 
     /**
@@ -116,6 +153,49 @@ class ConclusionController extends Controller
      */
     public function destroy(Conclusion $conclusion)
     {
-        //
+        Session::flash('success', 'Data Penutup tahun periode ' . $conclusion->setting->period . ' telah dihapus.');
+
+        if (Storage::exists('public/conclusion/' . $conclusion->attachment)) {
+            Storage::delete('public/conclusion/' . $conclusion->attachment);
+        }
+
+        $conclusion->delete();
+    }
+    
+    /**
+     * Yajra datatable
+     */
+    public function datatable(Request $request)
+    {
+        if ($request->ajax()) {
+            $conclusions = Conclusion::with('setting')->get();
+
+            return DataTables::of($conclusions)
+                ->addColumn('attachment', function ($conclusion) {
+                    return view('dashboard.conclusion.attachment', compact('conclusion'))->render();
+                })
+                ->addColumn('action', function ($conclusion) {
+                    return view('dashboard.conclusion.action', compact('conclusion'))->render();
+                })
+                ->rawColumns(['attachment', 'action'])
+                ->addIndexColumn()
+                ->make(true);
+        }
+    }
+
+    /**
+     * Select2
+     */
+    public function select2_period(Request $request)
+    {
+        if ($request->ajax()) {
+            $q = $request->input('q');
+
+            $assets = Setting::select('id', 'period')
+                ->where('period', 'LIKE', '%' . $q . '%')
+                ->get();
+
+            return response()->json($assets, 200);
+        }
     }
 }
