ea97c2fd9e0f6a5d908a2951c9208210e8a0323d
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-01-08 01:05:38 +0800

    add setting_id foreign in fundings table

--- database/migrations/2022_09_19_013807_create_fundings_table.php
+++ database/migrations/2022_09_19_013807_create_fundings_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->date('date_activity');
             $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('setting_id')->nullable()->constrained('settings')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
