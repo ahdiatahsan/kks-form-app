36c3fb50c71f601f69f2b037087c703d26ef748f
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-01-19 10:06:45 +0800

    add setting_id values in seeders

--- database/seeders/TatananThreeSeeder.php
+++ database/seeders/TatananThreeSeeder.php
@@ -17,73 +17,96 @@ class TatananThreeSeeder extends Seeder
     {
         $TatananThrees = [
             [
-                'user_id' => 1
+                'user_id' => 1,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 2
+                'user_id' => 2,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 3
+                'user_id' => 3,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 4
+                'user_id' => 4,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 5
+                'user_id' => 5,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 6
+                'user_id' => 6,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 7
+                'user_id' => 7,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 8
+                'user_id' => 8,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 9
+                'user_id' => 9,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 10
+                'user_id' => 10,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 11
+                'user_id' => 11,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 12
+                'user_id' => 12,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 13
+                'user_id' => 13,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 14
+                'user_id' => 14,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 15
+                'user_id' => 15,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 16
+                'user_id' => 16,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 17
+                'user_id' => 17,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 18
+                'user_id' => 18,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 19
+                'user_id' => 19,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 20
+                'user_id' => 20,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 21
+                'user_id' => 21,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 22
+                'user_id' => 22,
+                'setting_id' => 1
             ],
             [
-                'user_id' => 23
+                'user_id' => 23,
+                'setting_id' => 1
             ],
         ];
 
