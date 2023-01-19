1912ad8f402b2975b7f4662acb8cd6c4ad89c6ca
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-01-19 09:53:27 +0800

    add setting_id foreign in note(s) tables

--- database/migrations/2022_10_04_012129_create_note_threes_table.php
+++ database/migrations/2022_10_04_012129_create_note_threes_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('attachment_img')->nullable();
             $table->text('note')->nullable();
             $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('setting_id')->nullable()->constrained('settings')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
