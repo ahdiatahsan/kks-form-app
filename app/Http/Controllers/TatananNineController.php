aee1160f7dbd669a54b26beb2b375cb3467c511f
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-02-07 02:18:04 +0800

    update tatanan 9 view and controller (filter)

--- app/Http/Controllers/TatananNineController.php
+++ app/Http/Controllers/TatananNineController.php
@@ -8,6 +8,7 @@ use App\Models\AttachmentNineNd;
 use App\Models\NoteNine;
 use App\Http\Requests\StoreTatananNineRequest;
 use App\Http\Requests\UpdateTatananNineRequest;
+use App\Models\Setting;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -30,10 +31,68 @@ class TatananNineController extends Controller
      */
     public function index()
     {
-        $tatananNine = TatananNine::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachNine = AttachmentNine::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachNineNd = AttachmentNineNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        return view('dashboard.indicator.tatanan9.index', compact('tatananNine','attachNine','attachNineNd'));
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', 1)->first();
+
+        session(['period' => 1]);
+
+        $tatananNine = TatananNine::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachNine = AttachmentNine::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachNineNd = AttachmentNineNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+
+        return view('dashboard.indicator.tatanan9.index', compact('settings', 'tatananNine', 'attachNine', 'attachNineNd', 'period'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function tatanan_filter(Request $request)
+    {
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', $request->filter_period)->first();
+
+        session(['period' => $period->id]);
+
+        if (!empty($request->filter_period)) {
+            $tatananNine = TatananNine::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachNine = AttachmentNine::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachNineNd = AttachmentNineNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+        } else {
+            $tatananNine = TatananNine::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachNine = AttachmentNine::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachNineNd = AttachmentNineNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+        }
+
+        return view('dashboard.indicator.tatanan9.index', compact('settings', 'tatananNine', 'attachNine', 'attachNineNd', 'period'));
     }
 
     /**
@@ -88,8 +147,17 @@ class TatananNineController extends Controller
      */
     public function update(Request $request, TatananNine $tatananNine)
     {
-        $attachNine = AttachmentNine::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachNineNd = AttachmentNineNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
+        $period = $request->session()->get('period', 1);
+        $year = Setting::where('id', '=', $period)->first();
+
+        $attachNine = AttachmentNine::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
+        $attachNineNd = AttachmentNineNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
 
         $num = 23;
         for ($i = 1; $i <= $num; ++$i) {
@@ -103,8 +171,8 @@ class TatananNineController extends Controller
                 $request->validate([
                     $field2 => 'required|file|max:3048|mimes:pdf'
                 ], [
-                    $field2.'.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
-                    $field2.'.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
+                    $field2 . '.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
+                    $field2 . '.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
                 ]);
 
                 $converted = Str::remove('_1', $field2);
@@ -115,14 +183,16 @@ class TatananNineController extends Controller
 
                 $attachFile = $request->file($field2);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Sembilan - ' . $field2 . '.' . $extension;
+                $attachName = 'Tatanan Sembilan (' . $year->period . ') - ' . $field2 . '.' . $extension;
                 Storage::putFileAs('public/attachmentNine', $attachFile, $attachName);
 
                 $attachNine->update([
                     $converted => $attachName,
                 ]);
 
-                $noteNinePdf = NoteNine::where('code', '=', $converted)->first();
+                $noteNinePdf = NoteNine::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteNinePdf->update([
                     'attachment_pdf' => $attachName,
                 ]);
@@ -134,8 +204,8 @@ class TatananNineController extends Controller
                 $request->validate([
                     $field3 => 'required|file|max:2048|mimes:jpeg,jpg,png,webp'
                 ], [
-                    $field3.'.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
-                    $field3.'.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
+                    $field3 . '.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
+                    $field3 . '.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
                 ]);
 
                 $converted = Str::remove('_2', $field3);
@@ -146,14 +216,16 @@ class TatananNineController extends Controller
 
                 $attachFile = $request->file($field3);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Sembilan - ' . $field3 . '.' . $extension;
+                $attachName = 'Tatanan Sembilan (' . $year->period . ') - ' . $field3 . '.' . $extension;
                 Storage::putFileAs('public/attachmentNine', $attachFile, $attachName);
 
                 $attachNineNd->update([
                     $converted => $attachName,
                 ]);
 
-                $noteNineImg = NoteNine::where('code', '=', $converted)->first();
+                $noteNineImg = NoteNine::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteNineImg->update([
                     'attachment_img' => $attachName,
                 ]);
@@ -166,7 +238,9 @@ class TatananNineController extends Controller
                     $field1 => $request->input($field1),
                 ]);
 
-                $noteNineQa = NoteNine::where('code', '=', $field1)->first();
+                $noteNineQa = NoteNine::where('code', '=', $field1)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $userId = Auth::user()->id;
                 $answer = substr($request->input($field1), 0, 1);
                 $score = substr($request->input($field1), 2, 4);
@@ -179,7 +253,7 @@ class TatananNineController extends Controller
             }
         }
 
-        return redirect()->route('tatananNine.index')->with('success', 'Jawaban Anda Di Tatanan 9 Berhasil Disimpan.');
+        return redirect()->back()->with('success', 'Jawaban Anda Di Tatanan 9 Berhasil Disimpan.');
     }
 
     /**
