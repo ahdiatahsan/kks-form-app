4b06667dcffd58aceed8a95da02ae2a69a5f37f0
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-02-07 02:16:16 +0800

    update tatanan 1 view and controller (filter)

--- app/Http/Controllers/TatananOneController.php
+++ app/Http/Controllers/TatananOneController.php
@@ -8,6 +8,7 @@ use App\Models\AttachmentOneNd;
 use App\Models\NoteOne;
 use App\Http\Requests\StoreTatananOneRequest;
 use App\Http\Requests\UpdateTatananOneRequest;
+use App\Models\Setting;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -30,10 +31,68 @@ class TatananOneController extends Controller
      */
     public function index()
     {
-        $tatananOne = TatananOne::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachOne = AttachmentOne::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachOneNd = AttachmentOneNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        return view('dashboard.indicator.tatanan1.index', compact('tatananOne','attachOne','attachOneNd'));
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', 1)->first();
+
+        session(['period' => 1]);
+
+        $tatananOne = TatananOne::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachOne = AttachmentOne::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachOneNd = AttachmentOneNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+
+        return view('dashboard.indicator.tatanan1.index', compact('settings', 'tatananOne', 'attachOne', 'attachOneNd', 'period'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function tatanan_filter(Request $request)
+    {
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', $request->filter_period)->first();
+
+        session(['period' => $period->id]);
+
+        if (!empty($request->filter_period)) {
+            $tatananOne = TatananOne::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachOne = AttachmentOne::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachOneNd = AttachmentOneNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+        } else {
+            $tatananOne = TatananOne::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachOne = AttachmentOne::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachOneNd = AttachmentOneNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+        }
+
+        return view('dashboard.indicator.tatanan1.index', compact('settings', 'tatananOne', 'attachOne', 'attachOneNd', 'period'));
     }
 
     /**
@@ -88,8 +147,17 @@ class TatananOneController extends Controller
      */
     public function update(Request $request, TatananOne $tatananOne)
     {
-        $attachOne = AttachmentOne::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachOneNd = AttachmentOneNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
+        $period = $request->session()->get('period', 1);
+        $year = Setting::where('id', '=', $period)->first();
+
+        $attachOne = AttachmentOne::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
+        $attachOneNd = AttachmentOneNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
 
         $num = 68;
         for ($i = 1; $i <= $num; ++$i) {
@@ -103,8 +171,8 @@ class TatananOneController extends Controller
                 $request->validate([
                     $field2 => 'required|file|max:3048|mimes:pdf'
                 ], [
-                    $field2.'.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
-                    $field2.'.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
+                    $field2 . '.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
+                    $field2 . '.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
                 ]);
 
                 $converted = Str::remove('_1', $field2);
@@ -115,14 +183,16 @@ class TatananOneController extends Controller
 
                 $attachFile = $request->file($field2);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Satu - ' . $field2 . '.' . $extension;
+                $attachName = 'Tatanan Satu (' . $year->period . ') - ' . $field2 . '.' . $extension;
                 Storage::putFileAs('public/attachmentOne', $attachFile, $attachName);
 
                 $attachOne->update([
                     $converted => $attachName,
                 ]);
 
-                $noteOnePdf = NoteOne::where('code', '=', $converted)->first();
+                $noteOnePdf = NoteOne::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteOnePdf->update([
                     'attachment_pdf' => $attachName,
                 ]);
@@ -134,8 +204,8 @@ class TatananOneController extends Controller
                 $request->validate([
                     $field3 => 'required|file|max:2048|mimes:jpeg,jpg,png,webp'
                 ], [
-                    $field3.'.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
-                    $field3.'.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
+                    $field3 . '.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
+                    $field3 . '.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
                 ]);
 
                 $converted = Str::remove('_2', $field3);
@@ -146,14 +216,16 @@ class TatananOneController extends Controller
 
                 $attachFile = $request->file($field3);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Satu - ' . $field3 . '.' . $extension;
+                $attachName = 'Tatanan Satu (' . $year->period . ') - ' . $field3 . '.' . $extension;
                 Storage::putFileAs('public/attachmentOne', $attachFile, $attachName);
 
                 $attachOneNd->update([
                     $converted => $attachName,
                 ]);
 
-                $noteOneImg = NoteOne::where('code', '=', $converted)->first();
+                $noteOneImg = NoteOne::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteOneImg->update([
                     'attachment_img' => $attachName,
                 ]);
@@ -166,7 +238,9 @@ class TatananOneController extends Controller
                     $field1 => $request->input($field1),
                 ]);
 
-                $noteOneQa = NoteOne::where('code', '=', $field1)->first();
+                $noteOneQa = NoteOne::where('code', '=', $field1)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $userId = Auth::user()->id;
                 $answer = substr($request->input($field1), 0, 1);
                 $score = substr($request->input($field1), 2, 4);
@@ -179,7 +253,7 @@ class TatananOneController extends Controller
             }
         }
 
-        return redirect()->route('tatananOne.index')->with('success', 'Jawaban Anda Di Tatanan 1 Berhasil Disimpan.');
+        return redirect()->back()->with('success', 'Jawaban Anda Di Tatanan 1 Berhasil Disimpan.');
     }
 
     /**
