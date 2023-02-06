041f327fc6564ac92934022bcb0f001114696fe8
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-02-07 02:16:47 +0800

    update tatanan 3 view and controller (filter)

--- app/Http/Controllers/TatananThreeController.php
+++ app/Http/Controllers/TatananThreeController.php
@@ -8,6 +8,7 @@ use App\Models\AttachmentThreeNd;
 use App\Models\NoteThree;
 use App\Http\Requests\StoreTatananThreeRequest;
 use App\Http\Requests\UpdateTatananThreeRequest;
+use App\Models\Setting;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -30,10 +31,68 @@ class TatananThreeController extends Controller
      */
     public function index()
     {
-        $tatananThree = TatananThree::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachThree = AttachmentThree::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachThreeNd = AttachmentThreeNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        return view('dashboard.indicator.tatanan3.index', compact('tatananThree','attachThree','attachThreeNd'));
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', 1)->first();
+
+        session(['period' => 1]);
+
+        $tatananThree = TatananThree::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachThree = AttachmentThree::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+        $attachThreeNd = AttachmentThreeNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', 1)
+            ->first();
+
+        return view('dashboard.indicator.tatanan3.index', compact('settings', 'tatananThree', 'attachThree', 'attachThreeNd', 'period'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function tatanan_filter(Request $request)
+    {
+        $settings = Setting::all();
+        $period = Setting::where('id', '=', $request->filter_period)->first();
+
+        session(['period' => $period->id]);
+
+        if (!empty($request->filter_period)) {
+            $tatananThree = TatananThree::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachThree = AttachmentThree::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+            $attachThreeNd = AttachmentThreeNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', $request->filter_period)
+                ->first();
+        } else {
+            $tatananThree = TatananThree::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachThree = AttachmentThree::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+            $attachThreeNd = AttachmentThreeNd::with('user')
+                ->where('user_id', '=', Auth::user()->id)
+                ->where('setting_id', '=', 1)
+                ->first();
+        }
+
+        return view('dashboard.indicator.tatanan3.index', compact('settings', 'tatananThree', 'attachThree', 'attachThreeNd', 'period'));
     }
 
     /**
@@ -88,8 +147,17 @@ class TatananThreeController extends Controller
      */
     public function update(Request $request, TatananThree $tatananThree)
     {
-        $attachThree = AttachmentThree::with('user')->where('user_id', '=', Auth::user()->id)->first();
-        $attachThreeNd = AttachmentThreeNd::with('user')->where('user_id', '=', Auth::user()->id)->first();
+        $period = $request->session()->get('period', 1);
+        $year = Setting::where('id', '=', $period)->first();
+
+        $attachThree = AttachmentThree::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
+        $attachThreeNd = AttachmentThreeNd::with('user')
+            ->where('user_id', '=', Auth::user()->id)
+            ->where('setting_id', '=', $period)
+            ->first();
 
         $num = 18;
         for ($i = 1; $i <= $num; ++$i) {
@@ -103,8 +171,8 @@ class TatananThreeController extends Controller
                 $request->validate([
                     $field2 => 'required|file|max:3048|mimes:pdf'
                 ], [
-                    $field2.'.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
-                    $field2.'.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
+                    $field2 . '.mimes' => 'Lampiran harus berupa berkas berjenis: pdf.',
+                    $field2 . '.max' => 'Lampiran tidak boleh lebih besar dari 3 MB'
                 ]);
 
                 $converted = Str::remove('_1', $field2);
@@ -115,14 +183,16 @@ class TatananThreeController extends Controller
 
                 $attachFile = $request->file($field2);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Tiga - ' . $field2 . '.' . $extension;
+                $attachName = 'Tatanan Tiga (' . $year->period . ') - ' . $field2 . '.' . $extension;
                 Storage::putFileAs('public/attachmentThree', $attachFile, $attachName);
 
                 $attachThree->update([
                     $converted => $attachName,
                 ]);
 
-                $noteThreePdf = NoteThree::where('code', '=', $converted)->first();
+                $noteThreePdf = NoteThree::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteThreePdf->update([
                     'attachment_pdf' => $attachName,
                 ]);
@@ -134,8 +204,8 @@ class TatananThreeController extends Controller
                 $request->validate([
                     $field3 => 'required|file|max:2048|mimes:jpeg,jpg,png,webp'
                 ], [
-                    $field3.'.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
-                    $field3.'.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
+                    $field3 . '.mimes' => 'Dokumentasi harus berupa berkas berjenis: jpeg, jpg, png, webp.',
+                    $field3 . '.max' => 'Dokumentasi tidak boleh lebih besar dari 2 MB'
                 ]);
 
                 $converted = Str::remove('_2', $field3);
@@ -146,14 +216,16 @@ class TatananThreeController extends Controller
 
                 $attachFile = $request->file($field3);
                 $extension = $attachFile->extension();
-                $attachName = 'Tatanan Tiga - ' . $field3 . '.' . $extension;
+                $attachName = 'Tatanan Tiga (' . $year->period . ') - ' . $field3 . '.' . $extension;
                 Storage::putFileAs('public/attachmentThree', $attachFile, $attachName);
 
                 $attachThreeNd->update([
                     $converted => $attachName,
                 ]);
 
-                $noteThreeImg = NoteThree::where('code', '=', $converted)->first();
+                $noteThreeImg = NoteThree::where('code', '=', $converted)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $noteThreeImg->update([
                     'attachment_img' => $attachName,
                 ]);
@@ -166,7 +238,9 @@ class TatananThreeController extends Controller
                     $field1 => $request->input($field1),
                 ]);
 
-                $noteThreeQa = NoteThree::where('code', '=', $field1)->first();
+                $noteThreeQa = NoteThree::where('code', '=', $field1)
+                    ->where('setting_id', '=', $period)
+                    ->first();
                 $userId = Auth::user()->id;
                 $answer = substr($request->input($field1), 0, 1);
                 $score = substr($request->input($field1), 2, 4);
@@ -179,7 +253,7 @@ class TatananThreeController extends Controller
             }
         }
 
-        return redirect()->route('tatananThree.index')->with('success', 'Jawaban Anda Di Tatanan 3 Berhasil Disimpan.');
+        return redirect()->back()->with('success', 'Jawaban Anda Di Tatanan 3 Berhasil Disimpan.');
     }
 
     /**
